70224e66ba31e6ef22d2e581a68a1c088cda4dbd
Pradeep Reddy <92353826+PradeepChada@users.noreply.github.com>
2021-10-14 12:38:03 +0530

    179758889 -Banner with Home icon and testcases

--- src/__tests__/components/Header.test.js
+++ src/__tests__/components/Header.test.js
@@ -1,12 +1,29 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Header from '../../components/header/Header';
 
+jest.mock('react-router-dom', () => ({
+  useHistory: () => ({
+    push: jest.fn(),
+  }),
+}));
+
 describe('Testing Header component', () => {
+  test('should render a snapshot', () => {
+    const { container } = render(<Header />);
+    expect(container).toMatchSnapshot();
+  });
+
   test('should render Home Icon and Logo', () => {
     render(<Header />);
     const logo = screen.getByAltText('The Container Store');
     expect(logo).toBeInTheDocument();
     const homeIcon = screen.getByTestId('home-icon');
     expect(homeIcon).toBeInTheDocument();
+  });
+
+  test('should navidate to homepage when click on logo', () => {
+    render(<Header />);
+    const homeIcon = screen.getByTestId('home-icon');
+    fireEvent.click(homeIcon);
   });
 });
