893499d29d1066876578e6eaf0c3a443f7a1193a
rohit501688 <501668@example.com>
2021-10-21 19:49:10 +0530

    180026692 - unknown error design fixed

--- src/components/sku-error/SkuError.styles.js
+++ src/components/sku-error/SkuError.styles.js
@@ -4,28 +4,27 @@ import Box from '@mui/material/Box';
 import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
 import { colors } from '../../utils/themeUtils';
 
-
 export const TextWrapper = styled(Box)({
-    // width: '260px',
-    // marginTop: '30px',
+  // width: '260px',
+  // marginTop: '30px',
 });
 
 export const ErrorIcon = styled(ErrorOutlineIcon)({
-    color: colors.red,
-    width: '40px',
-    height: '40px',
-    marginBottom: '23px',
+  color: colors.red,
+  width: '48px',
+  height: '48px',
+  marginBottom: '16px',
 });
 
 export const Title = styled(Typography)({
-    fontSize: '18px',
-    fontWeight: '700',
-    color: colors.fontColor,
+  fontSize: '18px',
+  fontWeight: '700',
+  color: colors.fontColor,
 });
 
 export const Description = styled(Typography)({
-    fontSize: '16px',
-    fontWeight: '400',
-    textAlign: 'center',
-    color: colors.fontColor,
+  fontSize: '16px',
+  fontWeight: '400',
+  textAlign: 'center',
+  color: colors.fontColor,
 });
