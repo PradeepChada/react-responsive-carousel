9d9ecc8e0ef1ff63ac7fd721ceb83f919a586868
alffrey <alfchemm@publicisgroupe.net>
2021-11-11 13:16:40 -0600

    feat: reformatting /health response

--- server/routes/health.js
+++ server/routes/health.js
@@ -1,5 +1,15 @@
 const getHealth = (req, res) => {
-  res.status(200).json({ status: "NORMAL" });
+  res.status(200).json({
+    health: {
+      status: 'NORMAL',
+      healthAssessments: []
+    },
+    _links: {
+      self: {
+        href: '/health'
+      }
+    }
+  });
 };
 
 module.exports = {
