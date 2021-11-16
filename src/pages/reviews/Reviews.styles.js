4c3a9e163698c10cd68316da98be8928c44690f3
Pradeep Reddy <92353826+PradeepChada@users.noreply.github.com>
2021-11-16 21:06:19 +0530

    Removed unused code

--- src/pages/reviews/Reviews.styles.js
+++ src/pages/reviews/Reviews.styles.js
@@ -137,17 +137,6 @@ export const ReviewDetails = styled(Box)({
       marginRight: 10,
     },
   },
-  '& .up-vote': {
-    backgroundColor: colors.brandDarkBlue,
-    boxShadow: 'none',
-    marginTop: '1rem',
-    fontSize: '0.8rem',
-    padding: '4px 12px',
-    textTransform: 'none',
-    '& svg': {
-      fontSize: '1rem',
-    },
-  },
 });
 
 export const UpVoteBtn = styled(Button)({
