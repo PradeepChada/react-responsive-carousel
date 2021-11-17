6ee2e4a437abf83fb96a26e22b626286824964ba
Pradeep Reddy <92353826+PradeepChada@users.noreply.github.com>
2021-11-17 13:25:03 +0530

    180327001 - Fixed mui theme overrides

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { ThemeProvider, styled } from '@mui/material/styles';
+import {
+  ThemeProvider,
+  styled,
+  StyledEngineProvider,
+} from '@mui/material/styles';
 import HomeContainer from './pages/home/HomePage';
 import ProductDetails from './pages/product-details/ProductDetails';
 import SearchContainer from './pages/sku-search/SearchPage';
@@ -28,41 +32,47 @@ const App = () => {
   return (
     <Provider store={store}>
       <ConfigProvider>
-        <ThemeProvider theme={theme}>
-          <BrowserRouter>
-            <StoreProvider>
-              <Spinner />
-              <Header />
-              <StyledBody>
-                <Switch>
-                  <Route exact path='/' component={HomeContainer} />
-                  <Route exact path='/sku-search' component={SearchContainer} />
-                  <Route
-                    exact
-                    path='/product-details/:id'
-                    component={ProductDetails}
-                  />
-                  <Route
-                    exact
-                    path='/product-info/:id'
-                    component={ProductInfo}
-                  />
-                  <Route
-                    exact
-                    path='/product-variants/:id/:defaultProduct'
-                    component={ProductVariants}
-                  />
-                  <Route
-                    exact
-                    path='/sku-info/q&a/:id'
-                    component={QuestionAndAnswer}
-                  />
-                  <Route exact path='/reviews/:id' component={Reviews} />
-                </Switch>
-              </StyledBody>
-            </StoreProvider>
-          </BrowserRouter>
-        </ThemeProvider>
+        <StyledEngineProvider injectFirst>
+          <ThemeProvider theme={theme}>
+            <BrowserRouter>
+              <StoreProvider>
+                <Spinner />
+                <Header />
+                <StyledBody>
+                  <Switch>
+                    <Route exact path='/' component={HomeContainer} />
+                    <Route
+                      exact
+                      path='/sku-search'
+                      component={SearchContainer}
+                    />
+                    <Route
+                      exact
+                      path='/product-details/:id'
+                      component={ProductDetails}
+                    />
+                    <Route
+                      exact
+                      path='/product-info/:id'
+                      component={ProductInfo}
+                    />
+                    <Route
+                      exact
+                      path='/product-variants/:id/:defaultProduct'
+                      component={ProductVariants}
+                    />
+                    <Route
+                      exact
+                      path='/sku-info/q&a/:id'
+                      component={QuestionAndAnswer}
+                    />
+                    <Route exact path='/reviews/:id' component={Reviews} />
+                  </Switch>
+                </StyledBody>
+              </StoreProvider>
+            </BrowserRouter>
+          </ThemeProvider>
+        </StyledEngineProvider>
       </ConfigProvider>
     </Provider>
   );
