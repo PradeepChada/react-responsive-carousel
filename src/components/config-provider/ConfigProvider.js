d5e2a9ea55d82ff262644a5b4bec33c13cdf072e
Pradeep Reddy <92353826+PradeepChada@users.noreply.github.com>
2021-11-23 12:30:58 +0530

    179381436 - Added Spnner as expirence in delay.

--- src/components/config-provider/ConfigProvider.js
+++ src/components/config-provider/ConfigProvider.js
@@ -1,16 +1,23 @@
+import { CircularProgress, Typography } from '@mui/material';
+import { Box } from '@mui/system';
 import { useEffect, useState } from 'react';
 import config from '../../config';
 
 const ConfigProvider = ({ children }) => {
   const [configSuccess, setConfigSuccess] = useState(false);
   const [configError, setConfigError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
   const fetchConfig = () => {
+    setLoading(true);
     config
       .initializeAppConfig()
       .then(() => {
+        setLoading(false);
         setConfigSuccess(true);
       })
       .catch(() => {
+        setLoading(false);
         setConfigError(true);
       });
   };
@@ -18,8 +25,23 @@ const ConfigProvider = ({ children }) => {
   useEffect(() => {
     fetchConfig();
   }, []);
-
-  if (configSuccess) {
+  if (loading) {
+    return (
+      <Box
+        display={'flex'}
+        height='100vh'
+        flexGrow={1}
+        alignItems='center'
+        flexDirection='column'
+        justifyContent='center'
+      >
+        <CircularProgress />
+        <Typography variant='caption' sx={{ marginTop: 2 }}>
+          Loading Configurations...
+        </Typography>
+      </Box>
+    );
+  } else if (configSuccess) {
     return children;
   } else {
     return configError ? 'Config Error' : null;
