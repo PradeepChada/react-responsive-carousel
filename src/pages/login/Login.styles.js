51bfd4260af440738c99fc0d1083d2007fded17b
Pradeep Reddy <92353826+PradeepChada@users.noreply.github.com>
2021-12-22 16:21:41 +0530

    1795454041 - Removed unused styles

--- src/pages/login/Login.styles.js
+++ src/pages/login/Login.styles.js
@@ -27,26 +27,3 @@ export const PageContainer = styled(Container)({
     fontSize: font.size[14],
   },
 });
-
-export const Wrapper = styled(Box)({
-  marginTop: styles.margin[4],
-  marginBottom: styles.margin[3],
-  borderBottom: `1px solid ${colors.gray85}`,
-  '& ul': {
-    paddingLeft: '1.75rem',
-    '& li': {
-      color: colors.black,
-      fontSize: font.size[14],
-    },
-  },
-});
-
-export const Title = styled(Typography)({
-  fontSize: font.size[18],
-  fontWeight: font.weight[600],
-  marginBottom: styles.margin[3],
-});
-
-export const ErrorWrapper = styled(Box)({
-  padding: '1.875rem 0',
-});
